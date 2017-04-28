5e47db88bf7398bfd26c4b2cc063836a77911ad2
gartelgarth <gartel_garth@hotmail.com>
2017-04-28 08:33:55 -0400

    Add files via upload

---
+++ SpaceMain.cpp
@@ -0,0 +1,14 @@
+#include "Coord.h"
+#include "ExtraTerrestre.h"
+#include"Laser.h"
+#include"Martien.h"
+#include"Vaisseau.h"
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+
+	return 0;
+}
