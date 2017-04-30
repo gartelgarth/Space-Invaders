9fb32252441421abf0cf13f61016bdeffd531ac3
gartelgarth <gartel_garth@hotmail.com>
2017-04-30 19:15:41 -0400

    Add files via upload

--- SpaceMain.cpp
+++ SpaceMain.cpp
@@ -1,36 +1,69 @@
-#include "Vaisseau.h"
-#include "Laser.h"
-#include "Martien.h"
+#include "Coord.h"
 #include "ExtraTerrestre.h"
+#include"Laser.h"
+#include"Martien.h"
+#include"Vaisseau.h"
+#include<iostream>
 #include "UIKit.h"
 #include <Windows.h>
 #include <conio.h>
-#include "Timer.h"
+#include <time.h>
 using namespace std;
 
+class Timer {
+private:
+	long timer;
+	long delai;
 
+public:
+	Timer(long delai) {
+		this->delai = delai;
+	}
 
+	void start() {
+		this->timer = clock();
+	}
 
-#define MAX_LASERS 10
+	bool estPret() {
+		bool pret = false;
+
+		if (clock() >= timer + delai) {
+			pret = true;
+			timer = clock();
+		}
+
+		return pret;
+	}
+};
+
+#define MAX_LASERS 5
 #define MAX_INVADERS 5
 #define nbColonnes 70
 #define nbLignes 30
 
 int main()
 {
+	Martien * lesaliens[MAX_INVADERS];
+
+	for (int i = 0; i < MAX_INVADERS; i++) {
+		lesaliens[i] = new Martien((int)'x', 5);
+		lesaliens[i]->resetExtraTerrestre();
+	}
+
 	UIKit::curseurVisible(false);
 
 	bool finDeJeu = false;
 	UIKit::setDimensionFenetre(0, 0, nbColonnes + 30, nbLignes + 10);
 	UIKit::cadre(0, 0, nbColonnes, nbLignes, 50);
 	UIKit::cadre(71, 0, 90, nbLignes, 200);
-	UIKit::color(1);
+	UIKit::color(0004);
 	//	le vaisseau du joueur
 	Vaisseau monVaisseau;
-	Martien invader(5,6);
+	Martien invader(5, 6);
 	Timer timerVaisseau(100);
+	Timer timerJiggle(400);
 
-	//	un laser (pas Ã  l'Ã©cran au dÃ©but)
+	//	un laser (pas à l'écran au début)
 	Laser mesLasers[MAX_LASERS];
 	for (int i = 0; i < MAX_LASERS; i++)
 		mesLasers[i].isAlive = false;
@@ -53,27 +86,50 @@ int main()
 				}
 			}
 			else {
-				monVaisseau.modifierPosition(touche);
+				Coord vaisseauCoord = monVaisseau.coord;
+				if (touche == 'k' && vaisseauCoord.getPositionX() > 1
+					|| touche == 'l' && vaisseauCoord.getPositionX() < 69) {
+					monVaisseau.modifierPosition(touche);
+				}
 			}
 		}
-		//	on dÃ©place le laser
+		//	on déplace le laser
 		if (timerLaser.estPret())
 		{
 			for (int i = 0; i < MAX_LASERS; i++)
 			{
+				if (mesLasers[i].coord.getPositionY() == 1)
+				{
+					mesLasers[i].killLaser();
+				}
 				if (mesLasers[i].isAlive)
 				{
 					mesLasers[i].moveLaser();
 				}
 			}
-		}	
-		ExtraTerrestre * lesaliens[MAX_INVADERS];
-		lesaliens[MAX_INVADERS] = new Martien(5, 5);
-		lesaliens[MAX_INVADERS]->resetExtraTerrestre();
-		lesaliens[MAX_INVADERS]->putExtraTerrestre();
+		}
+		if (timerJiggle.estPret()) {
+			for (int i = 0; i < MAX_INVADERS; i++) {
+				if (lesaliens[i]->isAlive) {
+					lesaliens[i]->jiggleMartien();
+				}
+			}
+		}
 
-		
-
+		for (int i = 0; i < MAX_LASERS; i++) {
+			if (mesLasers[i].isAlive) {
+				Coord laserCoord = mesLasers[i].coord;
+				for (int j = 0; j < MAX_INVADERS; j++) {
+					if (lesaliens[j]->isAlive) {
+						Coord alienCoord = lesaliens[j]->coord;
+						if (laserCoord.getPositionX() == alienCoord.getPositionX() && laserCoord.getPositionY() == alienCoord.getPositionY()) {
+							lesaliens[j]->isAlive = false;
+							mesLasers[i].killLaser();
+						}
+					}
+				}
+			}
+		}
 	}
 
 	return 0;
